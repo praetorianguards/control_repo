a989de6cd54ec3357c1f51d591397c6a6e944bff
praetorianguards <32553356+praetorianguards@users.noreply.github.com>
2020-02-25 17:06:20 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDChS606O+fjuPd4gUx99dXTnvoMgjxSWPldL+SrOyXaCKXWzPfakpqLHdNjWxpTC7ugqeiV5BGUTzHpNzDRfaCMcXtbkOjUZxgHptkUhP6r5oe6eAX8+GTIMRU8JjyqaSdkNhqeGXdImRBf6giQ/tcniCiq6HuRaBgfdjaLgl86++57tSDKZnp2bE2Wg0RYLTBPnRHtCp5p03HC9/YAo0V3UevuLqLeYSb2PeyqUTTJcC6VkdvuThN2oNP7S6rEFQxv9yrEDA6IoL/jmmhgxaMYHEcvUN+L2bZibN7Txbp3qKYrZbVWs/6pepelkTHNL6OY+zr2JKTyzcs9ArI1K6x',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDChS606O+fjuPd4gUx99dXTnvoMgjxSWPldL+SrOyXaCKXWzPfakpqLHdNjWxpTC7ugqeiV5BGUTzHpNzDRfaCMcXtbkOjUZxgHptkUhP6r5oe6eAX8+GTIMRU8JjyqaSdkNhqeGXdImRBf6giQ/tcniCiq6HuRaBgfdjaLgl86++57tSDKZnp2bE2Wg0RYLTBPnRHtCp5p03HC9/YAo0V3UevuLqLeYSb2PeyqUTTJcC6VkdvuThN2oNP7S6rEFQxv9yrEDA6IoL/jmmhgxaMYHEcvUN+L2bZibN7Txbp3qKYrZbVWs/6pepelkTHNL6OY+zr2JKTyzcs9ArI1K6x',
   }  
 }
